cc6811a6b4919ab309ae379c0ffcca0e8567953d
Will the coder <w2gillette@gmail.com>
2022-10-22 06:31:17 -0500

    Code Wars entry - 2022.10.22

---
+++ 2022_10_22_uploads/2022_10_22_A.js
@@ -0,0 +1,8 @@
+// 8th kyu - Function 2 - squaring an argument
+// https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+// Write a function that takes an argument and returns the square of it
+//
+// will_the_coder - October 22, 2022
+//
+
+const square = (n) => n * n;
