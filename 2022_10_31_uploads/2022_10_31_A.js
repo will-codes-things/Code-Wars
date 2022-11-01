a46260491862673f59aff30eabb0723090ea0de6
Will the coder <w2gillette@gmail.com>
2022-11-01 02:42:38 -0500

    Code Wars entry - 2022.10.31

---
+++ 2022_10_31_uploads/2022_10_31_A.js
@@ -0,0 +1,21 @@
+// 8th kyu - Grasshopper - Messi Goals
+// https://www.codewars.com/kata/55ca77fa094a2af31f00002a/train/javascript
+// 
+// Messi's Goal Total
+// 
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+// Information
+// 
+// Messi goal scoring statistics:
+// Competition 	Goals
+// La Liga 	43
+// Champions League 	10
+// Copa del Rey 	5
+//
+// will_the_coder - October 30, 2022
+//
+
+let laLigaGoals = 43;
+let championsLeagueGoals = 10;
+let copaDelReyGoals = 5;
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
