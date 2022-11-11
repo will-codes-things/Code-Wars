c749104257e7170b7d68fa93d39098a90e5b87a1
Will the coder <w2gillette@gmail.com>
2022-11-10 21:20:49 -0600

    Code Wars secondary entry 2022.11.10

---
+++ 2022_11_10_uploads/2022_11_10_B.js
@@ -0,0 +1,8 @@
+// 8th kyu - Remove exclamation marks
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+// 
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+// 
+// will_the_coder - November 10, 2022
+
+const removeExclamationMarks = s => s.replace(/!/g,"");
