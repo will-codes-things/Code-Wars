5053134bad63b5804b297764d5fc65f9b7189281
William Gillette Jr <seclusion@gmail.com>
2022-11-27 21:28:02 -0600

    Code Wars entry - 22.11.27

---
+++ 2022_11_27_uploads/2022_11_27_A.js
@@ -0,0 +1,10 @@
+// 8th kyu - Localize The Barycenter of a Triangle
+// https://www.codewars.com/kata/5601c5f6ba804403c7000004/train/javascript
+//
+//The function bar_triang() or barTriang or bar-triang, receives the coordinates of the three vertices A, B and C as three different arguments and outputs the coordinates of the barycenter O in an array [xO, yO]
+//
+//This is how our asked function should work: the result of the coordinates should be expressed up to four decimals, (rounded result).
+//
+// will_the_coder - November 27, 2022
+
+const none = (arr, fun) => !arr.some(fun);
