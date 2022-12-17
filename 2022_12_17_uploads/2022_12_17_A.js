2739780fba495d3e1e9a028bb82b0bd6863cbeb2
Will the coder <w2gillette@gmail.com>
2022-12-17 03:46:29 -0600

    Code Wars Entry - 22.12.17

---
+++ 2022_12_17_uploads/2022_12_17_A.js
@@ -0,0 +1,12 @@
+// 8th kyu - Subtract the sum
+// https://www.codewars.com/kata/56c5847f27be2c3db20009c3
+//
+// Complete the function which get an input number n such that n >= 10 and n < 10000, then:
+// 
+// Sum all the digits of n.
+// Subtract the sum from n, and it is your new n.
+// If the new n is in the list below return the associated fruit, otherwise return back to task 1.
+//
+// will_the_coder - December 17, 2022
+
+const SubtractSum = () => "apple";
