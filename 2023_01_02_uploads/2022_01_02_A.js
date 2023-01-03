ae98197d64f581550434ccf76e4175bc43673804
Will the coder <w2gillette@gmail.com>
2023-01-02 18:25:43 -0600

    Code Wars entry - 23.01.02

---
+++ 2023_01_02_uploads/2022_01_02_A.js
@@ -0,0 +1,7 @@
+// 8th kyu - Powers of 2
+// https://www.codewars.com/kata/57a083a57cb1f31db7000028
+//
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+//
+// will_the_coder - January 2, 2023
+
