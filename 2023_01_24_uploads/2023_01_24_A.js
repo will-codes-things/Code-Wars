0eec6f4cc05f1ce7a64fab6caab97f06c484a048
Will the coder <w2gillette@gmail.com>
2023-01-24 20:32:43 -0600

    Code Wars entry - 2023.01.24

---
+++ 2023_01_24_uploads/2023_01_24_A.js
@@ -0,0 +1,8 @@
+// 8th kyu - String repeat
+// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+//
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+//
+// will_the_coder - January 24, 2023
+
+repeatStr = (n,s) => s.repeat(n)
