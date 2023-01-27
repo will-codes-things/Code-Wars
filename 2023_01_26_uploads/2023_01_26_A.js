d4f0d400110d825febfe28600db0fbdd01512bca
Will the coder <w2gillette@gmail.com>
2023-01-26 21:37:07 -0600

    Code Wars entry - 2023.01.26

---
+++ 2023_01_26_uploads/2023_01_26_A.js
@@ -0,0 +1,8 @@
+// 8th kyu - Convert a Boolean to a String
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/solutions/javascript
+//
+// Implement a function which convert the given boolean value into its string representation.
+//
+// will_the_coder - January 26, 2023
+
+const booleanToString = (b) => b.toString();
---
+++ 2023_01_26_uploads/2023_01_26_B.js
@@ -0,0 +1,22 @@
+// 8th kyu - How many lightsabers do you own?
+// https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
+//
+// Inspired by the development team at Vooza, write the function that
+// 
+//     accepts the name of a programmer, and
+//     returns the number of lightsabers owned by that person.
+// 
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+// 
+// Note: your function should have a default parameter.
+// 
+// For example(Input --> Output):
+// 
+// "anyone else" --> 0
+// "Zach" --> 18
+// 
+// 
+//
+// will_the_coder - January 26, 2023
+
+const howManyLightsabersDoYouOwn = (name) => name = "Zach" ?  18 : 0;
