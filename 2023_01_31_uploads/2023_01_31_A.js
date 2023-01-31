71fdf0112b6e53ae3bbe1b0f247ffbf12c74e8c4
Will the coder <w2gillette@gmail.com>
2023-01-31 09:58:42 -0600

    Correction to 2023.01.31 code

--- 2023_01_31_uploads/2023_01_31_A.js
+++ 2023_01_31_uploads/2023_01_31_A.js
@@ -29,3 +29,21 @@
 //
 // will_the_coder - January 31, 2023
 
+function humanYearsCatYearsDogYears (humanYears) {
+    let catYears
+    let dogYears
+
+    if (humanYears == 1) {
+        catYears = 15;
+        dogYears = 15;
+    } else if (humanYears == 2) {
+        catYears = 24;
+        dogYears = 24; 
+    } else if (humanYears >= 3) {
+        catYears = 24 + ((humanYears -2) * 4);
+        dogYears = 24 + ((humanYears -2) * 5);
+    }
+
+    return [humanYears, catYears, dogYears];
+    
+}
