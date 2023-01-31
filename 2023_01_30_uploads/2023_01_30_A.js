c32900214824b03028af872ae107690c71757001
Will the coder <w2gillette@gmail.com>
2023-01-31 09:50:15 -0600

    Code Wars entries - 2023.01.30 / 2023.01.31

---
+++ 2023_01_30_uploads/2023_01_30_A.js
@@ -0,0 +1,14 @@
+// 8th kyu - Collatz Conjecture (3n+1)
+// https://www.codewars.com/kata/577a6e90d48e51c55e000217
+//
+// The Collatz conjecture (also known as 3n+1 conjecture) is a conjecture that applying the following algorithm to any number we will always eventually reach one:
+// 
+// [This is writen in pseudocode]
+// if(number is even) number = number / 2
+// if(number is odd) number = 3*number + 1
+// #Task
+// 
+// Your task is to make a function hotpo that takes a positive n as input and returns the number of times you need to perform this algorithm to get n = 1.
+//
+// will_the_coder - January 30, 2023
+
