36460ab8f3787dc7e0d6b39e03fa1c874fcf52e7
Will the coder <w2gillette@gmail.com>
2023-02-01 12:36:00 -0600

    Code Wars entry - 2023.02.1

---
+++ 2023_02_01_uploads/2023_02_01_A.js
@@ -0,0 +1,9 @@
+// 8th kyu - if..else and ternary operator
+// https://www.codewars.com/kata/57202aefe8d6c514300001fd
+// 
+// Task:
+// 
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
+//
+// will_the_coder - February 2, 2023
+
