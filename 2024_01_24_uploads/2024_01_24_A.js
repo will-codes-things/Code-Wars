32b7b497d8bc8ff869e250af777b04efcc91361c
Will the Coder <w2gillette@gmail.com>
2024-01-24 18:57:14 -0600

    Code Wars entry 2024_01_24

---
+++ 2024_01_24_uploads/2024_01_24_A.js
@@ -0,0 +1,8 @@
+// 8th kyu - easy logs
+// https://www.codewars.com/kata/5b68c7029756802aa2000176/solutions
+// 
+// Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+// 
+// will_the_coder - January 24, 2024
+
+const logs = (x,a,b) => Math.log(a*b) / Math.log(x)
